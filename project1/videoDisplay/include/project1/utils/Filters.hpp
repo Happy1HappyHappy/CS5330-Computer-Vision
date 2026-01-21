b525fb94c156a950f287c7f38030bc8ad84eb829
Claire Liu <happy1claire@gmail.com>
2026-01-20 23:38:04 -0800

    finish infra for blur

--- project1/videoDisplay/include/project1/utils/Filters.hpp
+++ project1/videoDisplay/include/project1/utils/Filters.hpp
@@ -7,6 +7,10 @@ class Filters
 public:
     static int greyscale(cv::Mat &src, cv::Mat &dst);
     static int sepia(cv::Mat &src, cv::Mat &dst);
+    static int blur5x5_1( cv::Mat &src, cv::Mat &dst );
+    static int blur5x5_2( cv::Mat &src, cv::Mat &dst );
+private:
+    static double getTime();
 };
 
 #endif // FILTERS_HPP
---
+++ project1/videoDisplay/src/core/imgDisplay.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        cerr << "Invalid input arguments" << endl;
+        return -1;
+    }
+
+    // Load image
+    cv::Mat src;
+    cv::Mat dst;
+    char filename[256];
+    strcpy(filename, argv[1]);
+
+    src = cv::imread(filename);
+    if (src.empty()) {
+        printf("Unable to read image %s\n", filename);
+        exit -1;
+    }
+
+    // Configure and show window 
+    cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+    
+    cv::imshow("Image", src);
+
+    // Wait for user's keypress
+    
+    while(true) {
+        char key = cv::waitKey(10);
+        if (key == 'q' || key == 'Q') break;
+        else if (key == 's' || key == 'S') {
+            cv::imwrite("output_image.png", img);
+            cout << "Image saved as output_image.png" << endl;
+        }
+        
+    }
+    
+    return 0;
+}
