6d75a37802e0b71f857321fa819ac826503726d2
Yujing Wei <sylverwei@gmail.com>
2026-01-20 23:26:06 -0800

    implement vignette to Sepia tone filter

--- project1/videoDisplay/include/project1/utils/Filters.hpp
+++ project1/videoDisplay/include/project1/utils/Filters.hpp
@@ -6,7 +6,8 @@ class Filters
 {
 public:
     static int greyscale(cv::Mat &src, cv::Mat &dst);
-    static int sepia(cv::Mat &src, cv::Mat &dst);
+    static int sepia(cv::Mat &src, cv::Mat &dst, bool vignetteFlag = false);
+    static float vignette(float x, float y, float centerX, float centerY, float radius);
 };
 
 #endif // FILTERS_HPP
