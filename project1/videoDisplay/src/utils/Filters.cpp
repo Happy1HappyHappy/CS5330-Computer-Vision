ba760a80b5e67ed856c905204bbc8f763d5af35f
Yujing Wei <sylverwei@gmail.com>
2026-01-20 23:58:08 -0800

    update blur to vidDisplay.cpp

--- project1/videoDisplay/src/utils/Filters.cpp
+++ project1/videoDisplay/src/utils/Filters.cpp
@@ -1,5 +1,5 @@
 #include "project1/utils/Filters.hpp"
-#include <cstdio> // a bunch of standard C/C++ functions like printf, scanf
+#include <cstdio>  // a bunch of standard C/C++ functions like printf, scanf
 #include <cstring> // C/C++ functions for working with strings
 #include <cmath>
 #include <sys/time.h>
@@ -116,24 +116,24 @@ float Filters::vignette(float x, float y, float centerX, float centerY, float ra
     }
 }
 
-int blur5x5_1( cv::Mat &src, cv::Mat &dst )
+int blur5x5_1(cv::Mat &src, cv::Mat &dst)
 {
     // set up the timing for version 1
     double startTime = getTime();
-    
+
     // end the timing
     double endTime = getTime();
 
     // compute the time per image
     double difference = (endTime - startTime) / Ntimes;
-      // print the results
-    printf("Time per image (1): %.4lf seconds\n", difference );
-    
+    // print the results
+    printf("Time per image (1): %.4lf seconds\n", difference);
+
     return 0;
 }
 
-int blur5x5_2( cv::Mat &src, cv::Mat &dst )
-{   
+int blur5x5_2(cv::Mat &src, cv::Mat &dst)
+{
     // set up the timing for version 2
     double startTime = getTime();
 
@@ -142,14 +142,15 @@ int blur5x5_2( cv::Mat &src, cv::Mat &dst )
 
     // compute the time per image
     double difference = (endTime - startTime) / Ntimes;
-      // print the results
-    printf("Time per image (2): %.4lf seconds\n", difference );
+    // print the results
+    printf("Time per image (2): %.4lf seconds\n", difference);
     return 0;
 }
 
-double getTime() {
-  struct timeval cur;
+double getTime()
+{
+    struct timeval cur;
 
-  gettimeofday( &cur, NULL );
-  return( cur.tv_sec + cur.tv_usec / 1000000.0 );
+    gettimeofday(&cur, NULL);
+    return (cur.tv_sec + cur.tv_usec / 1000000.0);
 }
